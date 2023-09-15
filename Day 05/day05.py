e294d7c7c417b70d194047804b0c03b85b95b6bc
SumeshKumarjn <117620920+SumeshKumarjn@users.noreply.github.com>
2023-09-15 22:58:04 +0530

    Add files via upload

---
+++ Day 05/day05.py
@@ -0,0 +1,20 @@
+print("Marvel Movie Character Creator")
+print("--")
+print()
+hangingaround = input("Do you like 'hanging around'?: ")
+if hangingaround == "yes":
+  print("Then you're Spider-Man!")
+else:
+  print("Then you're not Spider-Man!")
+print()
+gravellyvoice = input("Do you have a 'gravelly voice'?: ")
+if gravellyvoice == "yes":
+  print("Aww, then you're Korg!")
+else:
+  print("Aww, then you're not Korg!")
+print()
+marvelous = input("Do you often feel 'Marvelous'?: ")
+if marvelous == "yes":
+  print("Aha, You're Captain Amaerica!")
+else:
+  print("Aha, You're not Captain America!")
