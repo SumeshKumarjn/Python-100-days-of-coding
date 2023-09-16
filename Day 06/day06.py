594958ea35830902f473a5f55a9f9784d21edd1e
SumeshKumarjn <117620920+SumeshKumarjn@users.noreply.github.com>
2023-09-16 22:03:56 +0530

    Add files via upload

---
+++ Day 06/day06.py
@@ -0,0 +1,16 @@
+print("MY LOGIN SYSTEM")
+print("++++++++++++++++++")
+name = input("Username > ")
+security = input("Password > ")
+if name == "David" and security == "password":
+  print("Hey, David")
+elif name == "John" and security == "myself":
+  print("Welcome, John")
+else:
+  print("Go,away!")
+print()
+print("Hello there",name,", What a lovely accent you have you could have charmed your way in here even without a password.")
+print()
+print("Have a great day.")
+print()
+print("Don't forget to wear a hat in the sun!")
