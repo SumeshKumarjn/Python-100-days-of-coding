d76028191a45ff279f5d4f4ca6c94fb2e1cf925c
SumeshKumarjn <117620920+SumeshKumarjn@users.noreply.github.com>
2023-09-18 17:06:05 +0530

    Add files via upload

---
+++ Day 07/day07.py
@@ -0,0 +1,30 @@
+print("Fake Fan Finder 👀")
+print("------------------")
+print()
+favanime = input("Whats your favourite Anime? ")
+if favanime == "One Piece":
+  print("Oh really?!")
+  print()
+  favchar = input("Name me any of the characters? ")
+  if favchar == "Nami":
+    print("You got that by pure chance.Okay then, ")
+    print()
+    job = input("What is her job on the ship? ")
+    if job == "Dont Know":
+      print("See! Fake One Piece Fan!")
+    else:
+      print("You're not a fake fan")
+elif favanime == "Death Note":
+  print("Oh nice!")
+  print()
+  favchar = input("Name me any of the characters? ")
+  if favchar == "Light Yagami":
+    print("You got that by pure chance.Okay then, ")
+    print()
+    job = input("What is the role of this character? ")
+    if job == "Dont Know":
+      print("See! Fake One Piece Fan!")
+    else:
+      print("You're not a fake fan")
+else:
+  print("Yeah, thats cool :) ")
