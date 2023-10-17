6b9d6deb9b05196526526616a8112d661dc18218
SumeshKumarjn <117620920+SumeshKumarjn@users.noreply.github.com>
2023-10-18 00:15:00 +0530

    Add files via upload

---
+++ Day 20/day 20.py
@@ -0,0 +1,13 @@
+print("List Generator")
+print("--------------")
+print()
+start = int(input("Start at: "))
+print()
+end = int(input("End before: "))
+print()
+step = int(input("Increment between values: "))
+print()
+print("List: ")
+print()
+for i in range(start,end,step):
+  print(i)
